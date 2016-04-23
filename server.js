33f70ebbf52e07674e7226508691c7dc786467c0
Jiri Spac <capajj@gmail.com>
2016-04-23 12:52:40 +0200

    take port from env, so that this works on heroku

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
 var config = require('./webpack.config')
 
+const port = process.env.PORT || 3000
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, 'localhost', function (err, result) {
   if (err) {
     return console.log(err)
   }
